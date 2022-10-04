83068ff3293a028050520d97cb3d7669cc216d13
michaelmirandi <61252938+michaelmirandi@users.noreply.github.com>
2022-10-03 23:04:18 -0400

    Revert Auth Links Back

--- app/api/auth.py
+++ app/api/auth.py
@@ -29,8 +29,8 @@ auth_router = r = APIRouter()
 ##################################
 
 
-BASE_ERGOAUTH = "ergoauth://192.168.1.10:8000"
-BASE_URL = "http://192.168.1.10:8000"
+BASE_ERGOAUTH = "ergoauth://api.paideia.im"
+BASE_URL = "https://api.paideia.im"
 
 
 @r.post("/login", response_model=LoginRequestWebResponse, name="ergoauth:login-web")
