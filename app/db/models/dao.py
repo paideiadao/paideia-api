ffe114b718fd9d044c31533372b9250b18b73e87
michaelmirandi <61252938+michaelmirandi@users.noreply.github.com>
2022-10-25 07:39:38 -0400

    Vw Daos

--- app/db/models/dao.py
+++ app/db/models/dao.py
@@ -19,3 +19,12 @@ class Dao(Base):
     is_published = Column(Boolean)
     nav_stage = Column(Integer)
     is_review = Column(Boolean)
+
+class vw_daos(Base):
+    __tablename__ = "vw_activity_log"
+
+    id = Column(Integer, primary_key=True, index=True)
+    dao_name = Column(String)
+    dao_url = Column(String)
+    token_id = Column(String)
+    logo_url = Column(String)
