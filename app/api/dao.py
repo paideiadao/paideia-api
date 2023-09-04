746a407bf483bd7d4b31d1b9957434f951af5654
Robert Pieter van Leeuwen <luivatra@gmail.com>
2023-09-04 15:12:55 +0200

    dao desc

--- app/api/dao.py
+++ app/api/dao.py
@@ -54,7 +54,7 @@ def dao_list(
                         dao_config = dao.get_dao_config(d)
                         edit_dao(db, dbd.id, CreateOrUpdateDao(
                             dao_name=state_daos[d][0],
-                            dao_short_description=dao_config["im.paideia.dao.description"],
+                            dao_short_description=dao_config["im.paideia.dao.description"]["value"],
                             dao_url=state_daos[d][0],
                             dao_key=d,
                             governance=CreateOrUpdateGovernance(
@@ -63,7 +63,7 @@ def dao_list(
                                 support_needed=int(dao_config["im.paideia.dao.threshold"]["value"])
                             ),
                             tokenomics=CreateOrUpdateTokenomics(
-                                token_id=dao_config["im.paideia.dao.tokenid"]
+                                token_id=dao_config["im.paideia.dao.tokenid"]["value"]
                             ),
                             config_height=state_daos[d][1],
                             design=CreateOrUpdateDaoDesign(),
@@ -76,7 +76,7 @@ def dao_list(
                     dao_key=d,
                     config_height=state_daos[d][1],
                     dao_name=state_daos[d][0],
-                    dao_short_description=dao_config["im.paideia.dao.description"],
+                    dao_short_description=dao_config["im.paideia.dao.description"]["value"],
                     dao_url=state_daos[d][0],
                     governance=CreateOrUpdateGovernance(
                         quorum=int(dao_config["im.paideia.dao.quorum"]["value"]),
@@ -84,7 +84,7 @@ def dao_list(
                         support_needed=int(dao_config["im.paideia.dao.threshold"]["value"])
                     ),
                     tokenomics=CreateOrUpdateTokenomics(
-                        token_id=dao_config["im.paideia.dao.tokenid"]
+                        token_id=dao_config["im.paideia.dao.tokenid"]["value"]
                     ),
                     design=CreateOrUpdateDaoDesign(),
                     is_draft=False,
