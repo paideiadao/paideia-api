e87cb7fc2a2b973cea201fe213e83896ed254143
Robert Pieter van Leeuwen <luivatra@gmail.com>
2023-11-22 11:06:16 +0100

    switch to crux for tokeninfo

--- app/ergo/indexed_node_client.py
+++ app/ergo/indexed_node_client.py
@@ -7,11 +7,23 @@ def get_token_info(token_id: str):
     cached = cache.get("get_token_info_" + token_id)
     if cached:
         return cached
-    res = requests.get(Config[Network].node+":9053/blockchain/token/byId/"+token_id)
-    if res.ok:
-        token_info = res.json()
-        if "name" not in token_info:
-            token_info["name"] = ""
+    #res = requests.get(Config[Network].node+":9053/blockchain/token/byId/"+token_id)
+    res = requests.get("https://api.cruxfinance.io/crux/token_info/"+token_id)
+    if res.ok:      
+        #temp fix start#
+        resJson = res.json()
+        token_info = {
+        "id": token_id,
+        "boxId": token_id,
+        "emissionAmount": resJson["minted"],
+        "name": resJson["token_name"],
+        "description": resJson["token_description"],
+        "decimals": resJson["decimals"]
+        }
+        ##temp fix end##
+        
+        #token_info = res.json()
+        
         cache.set("get_token_info_" + token_id, token_info)
         return token_info
     else:
