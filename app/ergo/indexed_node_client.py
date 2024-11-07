e69a50148eae0b9dce43e0ff574a110ce68a1b68
Robert Pieter van Leeuwen <luivatra@gmail.com>
2024-11-07 21:08:07 +0100

    remove hardcoded node port

--- app/ergo/indexed_node_client.py
+++ app/ergo/indexed_node_client.py
@@ -30,21 +30,21 @@ def get_token_info(token_id: str):
         return None
     
 def get_balance(address: str):
-    res = requests.post(Config[Network].node+":9053/blockchain/balance", data=address)
+    res = requests.post(Config[Network].node+"/blockchain/balance", data=address)
     if res.ok:
         return res.json()
     else:
         return None
     
 def get_transactions(address: str, offset: int, limit: int):
-    res = requests.post(Config[Network].node+':9053/blockchain/transaction/byAddress?offset='+str(offset)+'&limit='+str(limit), data=address)
+    res = requests.post(Config[Network].node+'/blockchain/transaction/byAddress?offset='+str(offset)+'&limit='+str(limit), data=address)
     if res.ok:
         return res.json()
     else:
         return None
     
 def get_box_by_id(boxId: str):
-    res = requests.get(Config[Network].node+':9053/blockchain/box/byId/'+boxId)
+    res = requests.get(Config[Network].node+'/blockchain/box/byId/'+boxId)
     if res.ok:
         return res.json()
     else:
